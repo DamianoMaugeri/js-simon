e79a55c87f16f2c6277283b622164d5b0b4f5b51
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-27 17:25:55 +0100

    [fix]  inserito unico ciclo for

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // variabili
 
-const maxNumber = 50 
+const maxNumber = 50;
+
+const minNumber = 1;
 
 const startButton = document.getElementById('start-button')
 
@@ -10,11 +12,15 @@ const answerForm = document.getElementById('answers-form')
 
 const countdownEl = document.getElementById('countdown');
 
+const numeriInseriti = document.querySelectorAll('#input-group input')
+
+/*
 const numeroInserito1 = document.getElementById('n1')
 const numeroInserito2 = document.getElementById('n2')
 const numeroInserito3 = document.getElementById('n3')
 const numeroInserito4 = document.getElementById('n4')
 const numeroInserito5 = document.getElementById('n5')
+*/
 
 const counterV = document.getElementById('message-n-v')
 const messageV = document.getElementById('message-v')
@@ -24,27 +30,29 @@ const messageX = document.getElementById('message-x')
 const error = document.getElementById('error')
 
 
-const arrayNum = []; 
+const arrayNum = [];
 const arrayuser = [];
 const arraysoluzioniV = [];
 const arraysoluzioniX = [];
 let soluzioniV = 0;
-let numeriV ='';
+let numeriV = '';
 let soluzioniX = 0;
-let numeriX ='';
+let numeriX = '';
+
+const numeriCasualiDaGenerare = 5
 
 
 // al primo evento generare un countdown ,generare array di numeri che vengono visti come list item  alla fine del countdown mttere tutto d-none
 
-startButton.addEventListener('click',() =>{
+startButton.addEventListener('click', () => {
     startButton.disabled = true;
 
     // mi creo l'array 
 
 
-
-    for (let i=0; i < 5 ; i++){
-        let arrayElement = randomIntFromOneTo(maxNumber)
+/*
+    for (let i = 0; i < numeriCasualiDaGenerare; i++) {
+        let arrayElement = randomIntFromOneTo(minNumber,maxNumber)
         arrayNum.push(arrayElement)
 
         const liElement = document.createElement('li')
@@ -55,119 +63,96 @@ startButton.addEventListener('click',() =>{
     }
     console.log(arrayNum)
 
-    // mettere il form in d-flex
-    setTimeout(displayChange, 30 * 1000);
+*/
 
-    let number = 30;
-    const intervalId = setInterval(() => {        
-        number--;
-        countdownEl.innerHTML = number;
-        if (number === 0) {
+while(arrayNum.length < numeriCasualiDaGenerare){
+    let arrayElement = randomIntFromOneTo(minNumber,maxNumber)
+    
+    if(!arrayNum.includes(arrayElement)){
+        arrayNum.push(arrayElement)
+        const liElement = document.createElement('li')
+        liElement.innerHTML = arrayElement
+        numberList.appendChild(liElement)
+
+    }
+}
+
+console.log(arrayNum)
+
+
+    // mettere il form in d-flex
+    // setTimeout(displayChange, 30 * 1000);
+
+    let timerNumber = 3;
+    const intervalId = setInterval(() => {
+        timerNumber--;
+        countdownEl.innerHTML = timerNumber;
+        if (timerNumber === 0) {
+            displayChange()
             clearInterval(intervalId);
-        } 
+        }
     }, 1000);
 
 
 })
 
 
-
-
 // ricere gli input dal form 
 
-answerForm.addEventListener('submit', function(event){
+answerForm.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    // validazione dei numeri
+
+    const risposreUtente = []
+
+    for (let i = 0; i < numeriInseriti.length; i++) {
+        const currentElement = parseInt(numeriInseriti[i].value);
     
-    const n1 = parseInt(numeroInserito1.value)
-    const n2 = parseInt(numeroInserito2.value)
-    const n3 = parseInt(numeroInserito3.value)
-    const n4 = parseInt(numeroInserito4.value)
-    const n5 = parseInt(numeroInserito5.value)
+        if (
+          !isNaN(currentElement) && 
+          currentElement >= minNumber && 
+          currentElement <= maxNumber && 
+          !risposreUtente.includes(currentElement) 
+        ) {
+          risposreUtente.push(currentElement);
+        }
+      }
 
-    
-    // controllare se gli input corrispondono a valori nell'array e in caso salvarli  e far accrescere un contatore risposte giuste 
-
-    if(arrayNum.includes(n1)){
-        soluzioniV ++ ;
-        arraysoluzioniV.push(n1);
-
-    }else{
-        soluzioniX ++;
-        arraysoluzioniX.push(n1);
-    }
-
-
-    if(arrayNum.includes(n2)){
-        soluzioniV ++ ;
-        arraysoluzioniV.push(n2);
-
-    }else{
-        soluzioniX ++;
-        arraysoluzioniX.push(n2);
-    }
-
-
-    if(arrayNum.includes(n3)){
-        soluzioniV ++ ;
-        arraysoluzioniV.push(n3);
-
-    }else{
-        soluzioniX ++;
-        arraysoluzioniX.push(n3);
-    }
-
-
-    if(arrayNum.includes(n4)){
-        soluzioniV ++ ;
-        arraysoluzioniV.push(n4);
-    
-
-    }else{
-        soluzioniX ++;
-        arraysoluzioniX.push(n4);
-
-    }
-
-
-    if(arrayNum.includes(n5)){
-        soluzioniV ++ ;
-        arraysoluzioniV.push(n5);
-    
-    }else{
-        soluzioniX ++;
-        arraysoluzioniX.push(n5);
-
-    }
-
-    numeriV = arraysoluzioniV.join()
-    numeriX = arraysoluzioniX.join();
-    
-    // stampare le risposte giuste ed il contatore
-
-    if(isNaN(n1)||isNaN(n2)||isNaN(n3)||isNaN(n4)||isNaN(n5)){
+      if (risposreUtente.length < numeriCasualiDaGenerare){
 
         error.classList.remove('d-none')
         error.classList.add('d-flex')
         error.append('INSERISCI I DATI CORRETTI')
-        
 
-    }else{
+
+      } else{
+
+        for (let i=0; i<risposreUtente.length; i++){
+            
+            if (arrayNum.includes(risposreUtente[i])){
+
+                soluzioniV++;
+                arraysoluzioniV.push(risposreUtente[i]);
+
+            } else{
+                soluzioniX++;
+                arraysoluzioniX.push(risposreUtente[i])
+
+            }
+        }
+
+        numeriV = arraysoluzioniV.join();
+        numeriX = arraysoluzioniX.join();
+
         counterV.append(`soluzioni corrette: ${soluzioniV}`)
         messageV.append(`${numeriV}`)
         counterX.append(`soluzioni sbagliate: ${soluzioniX}`)
         messageX.append(`${numeriX}`)
 
-    }
+      }
 
-
-
-})
-
-
-
-
-
-
+    });
 
 
 
@@ -176,18 +161,20 @@ answerForm.addEventListener('submit', function(event){
 // funzioni
 
 
-function randomIntFromOneTo(maxNumber) {
-	const randomInt = Math.floor(Math.random() * maxNumber) + 1
-    
-    	return randomInt
+function randomIntFromOneTo(min,max) {
+   // const randomInt = Math.floor(Math.random() * maxNumber) + 1
+
+    const randomInt = Math.floor(Math.random() * (max - min+ 1)) + min;
+
+
+    return randomInt
 }
 
 
-function displayChange(){
+function displayChange() {
     numberList.classList.add('d-none')
     numberList.classList.remove('d-flex')
     answerForm.classList.add('d-block')
     answerForm.classList.remove('d-none')
-
 
 }
